a938dfc37fdd071139a149f9bd45da26438791da
Jan Almazora <jalmazora@ewise.com>
2016-11-12 01:51:09 +0800

    user maintenance.

--- app/components/user/user.component.ts
+++ app/components/user/user.component.ts
@@ -1,28 +1,166 @@
 import { Component, OnInit } from '@angular/core';
+
+import { PositionService } from '../position/position.service';
 import { UserService } from './user.service';
-import { SweetAlertService } from '../../shared-services/swal.service';
+import { SweetAlertService, ToastrService } from '../../shared-services/services';
+
+import { User, Position, Modal } from '../../models/model';
 
 @Component({
     selector: 'user-component',
     templateUrl: './app/components/user/user-page.html',
     providers: [
         UserService,
-        SweetAlertService
+        PositionService,
+        SweetAlertService,
+        ToastrService
     ]
 })
 
 export class UserComponent implements OnInit {
 
+    constructor(
+        private userService: UserService,
+        private positionService: PositionService,
+        private swal: SweetAlertService,
+        private toastr: ToastrService) { }
+
     ngOnInit() {
-        // this.swal.confirm("Are you sure?", "You won't be able to recover this.", (isConfirm) => {
 
-        // });
-    }
-
-    constructor(private userService: UserService, private swal: SweetAlertService) {
+        this.modal = new Modal("#mdlModalInfo");
+        this.getAllUsers();
+        this.getAllPositions();
 
     }
 
+    positions: Position[] = [];
+    users: User[] = [];
 
+    selectedUser: User;
+
+    loadingPositions: boolean;
+    loadingUsers: boolean;
+    loadingCompanies: boolean;
+    loadingEmploymentStatuses: boolean;
+    addingUser: boolean;
+    updatingUser: boolean;
+    deletingUser: boolean;
+
+    isFormDisabled: boolean;
+
+    modal: Modal;
+
+    operation: number = 0;
+
+    getAllUsers(): void {
+
+        try {
+
+            this.users = [];
+
+            this.loadingUsers = true;
+            this.isFormDisabled = true;
+
+            this.userService.getAll().then((result) => {
+
+                this.loadingUsers = false;
+                this.isFormDisabled = false;
+
+                if(result.success) {
+
+                    this.users = result.data as User[];
+                    this.toastr.success(result.message);
+                    
+                } else {
+
+                    this.toastr.error(result.message)
+
+                }
+
+            })
+            .catch((error) => {
+
+                this.loadingUsers = false;
+                this.isFormDisabled = false;
+                this.toastr.error(error);
+
+            });
+
+        } catch(e) {
+
+            this.loadingUsers = false;
+            this.isFormDisabled = false;
+            this.toastr.error(e);
+
+        }
+
+    }   
+
+    getAllPositions(): void {
+
+        try {
+
+            this.positions = [];
+
+            this.loadingPositions = true;
+            this.isFormDisabled = true;
+
+            this.positionService.getAll().then((result) => {
+
+                this.loadingPositions = false;
+                this.isFormDisabled = false;
+
+                if(result.success) {
+
+                    this.positions = result.data as Position[];
+                    this.toastr.success(result.message);
+
+                } else {
+
+                    this.toastr.error(result.message);
+
+                }
+
+            })
+            .catch((error) => {
+
+                this.loadingPositions = false;
+                this.isFormDisabled = false;
+                this.toastr.error(error);
+
+            });
+
+        } catch(e) {
+
+            this.loadingPositions = false;
+            this.isFormDisabled = false;
+            this.toastr.error(e);
+
+        }
+
+    }
+
+    add(): void {
+
+        this.operation = 1;
+        this.isFormDisabled = false;
+        this.selectedUser = new User();
+
+        this.selectedUser.position = new Position();
+        this.selectedUser.position._id = this.positions[0]._id;
+
+    }
+
+    confirmAdd(): void {
+
+        // todo
+
+    }
+
+    private addUser(): void {
+
+        // todo
+
+    }
 
 }
--- dist/components/user/user.component.js
+++ dist/components/user/user.component.js
@@ -9,16 +9,95 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var position_service_1 = require('../position/position.service');
 var user_service_1 = require('./user.service');
-var swal_service_1 = require('../../shared-services/swal.service');
+var services_1 = require('../../shared-services/services');
+var model_1 = require('../../models/model');
 var UserComponent = (function () {
-    function UserComponent(userService, swal) {
+    function UserComponent(userService, positionService, swal, toastr) {
         this.userService = userService;
+        this.positionService = positionService;
         this.swal = swal;
+        this.toastr = toastr;
+        this.positions = [];
+        this.users = [];
+        this.operation = 0;
     }
     UserComponent.prototype.ngOnInit = function () {
-        // this.swal.confirm("Are you sure?", "You won't be able to recover this.", (isConfirm) => {
-        // });
+        this.modal = new model_1.Modal("#mdlModalInfo");
+        this.getAllUsers();
+        this.getAllPositions();
+    };
+    UserComponent.prototype.getAllUsers = function () {
+        var _this = this;
+        try {
+            this.users = [];
+            this.loadingUsers = true;
+            this.isFormDisabled = true;
+            this.userService.getAll().then(function (result) {
+                _this.loadingUsers = false;
+                _this.isFormDisabled = false;
+                if (result.success) {
+                    _this.users = result.data;
+                    _this.toastr.success(result.message);
+                }
+                else {
+                    _this.toastr.error(result.message);
+                }
+            })
+                .catch(function (error) {
+                _this.loadingUsers = false;
+                _this.isFormDisabled = false;
+                _this.toastr.error(error);
+            });
+        }
+        catch (e) {
+            this.loadingUsers = false;
+            this.isFormDisabled = false;
+            this.toastr.error(e);
+        }
+    };
+    UserComponent.prototype.getAllPositions = function () {
+        var _this = this;
+        try {
+            this.positions = [];
+            this.loadingPositions = true;
+            this.isFormDisabled = true;
+            this.positionService.getAll().then(function (result) {
+                _this.loadingPositions = false;
+                _this.isFormDisabled = false;
+                if (result.success) {
+                    _this.positions = result.data;
+                    _this.toastr.success(result.message);
+                }
+                else {
+                    _this.toastr.error(result.message);
+                }
+            })
+                .catch(function (error) {
+                _this.loadingPositions = false;
+                _this.isFormDisabled = false;
+                _this.toastr.error(error);
+            });
+        }
+        catch (e) {
+            this.loadingPositions = false;
+            this.isFormDisabled = false;
+            this.toastr.error(e);
+        }
+    };
+    UserComponent.prototype.add = function () {
+        this.operation = 1;
+        this.isFormDisabled = false;
+        this.selectedUser = new model_1.User();
+        this.selectedUser.position = new model_1.Position();
+        this.selectedUser.position._id = this.positions[0]._id;
+    };
+    UserComponent.prototype.confirmAdd = function () {
+        // todo
+    };
+    UserComponent.prototype.addUser = function () {
+        // todo
     };
     UserComponent = __decorate([
         core_1.Component({
@@ -26,10 +105,12 @@ var UserComponent = (function () {
             templateUrl: './app/components/user/user-page.html',
             providers: [
                 user_service_1.UserService,
-                swal_service_1.SweetAlertService
+                position_service_1.PositionService,
+                services_1.SweetAlertService,
+                services_1.ToastrService
             ]
         }), 
-        __metadata('design:paramtypes', [user_service_1.UserService, swal_service_1.SweetAlertService])
+        __metadata('design:paramtypes', [user_service_1.UserService, position_service_1.PositionService, services_1.SweetAlertService, services_1.ToastrService])
     ], UserComponent);
     return UserComponent;
 }());
